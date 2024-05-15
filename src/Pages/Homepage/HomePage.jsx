8eb261afb7d91f425ec9fa54b8fa3dd2c4ed5be6
ACHMAD IRWANDA <achmad.irwanda@btpn.com>
2024-05-15 08:02:58 +0700

    feat: balance & transaction information in homepage

--- src/Pages/Homepage/HomePage.jsx
+++ src/Pages/Homepage/HomePage.jsx
@@ -2,8 +2,19 @@ import React, { useEffect, useState } from 'react';
 import { BACKEND_URL, CUSTOMER_ID } from '../../constants';
 import axios from 'axios';
 
+function getAmountTotalCategorized(transactions, type) {
+  return transactions.reduce((total, transaction) => {
+    const { amount } = transaction;
+    if (transaction.type === type) return total + amount;
+    return total;
+  }, 0);
+}
+
 export default function HomePage() {
   const [user, setUser] = useState();
+  const [wallet, setWallet] = useState();
+  const [depositAmount, setDepositAmount] = useState(0);
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
 
   useEffect(() => {
     async function fetchUserData() {
@@ -16,10 +27,58 @@ export default function HomePage() {
     fetchUserData();
   }, [CUSTOMER_ID]);
 
+  useEffect(() => {
+    async function fetchUserWallet() {
+      if (user) {
+        const { data } = await axios.get(
+          `${BACKEND_URL}/wallets/${user?.walletId}`
+        );
+        setWallet(data);
+      }
+    }
+
+    fetchUserWallet();
+  }, [user]);
+
+  useEffect(() => {
+    async function fetchUserTransactions() {
+      if (wallet) {
+        const { data } = await axios.get(
+          `${BACKEND_URL}/wallets/${wallet.id}/transactions`
+        );
+        const { transactions } = data;
+        const deposit = getAmountTotalCategorized(transactions, 'deposit');
+        const withdraw = getAmountTotalCategorized(transactions, 'withdraw');
+        setDepositAmount(deposit);
+        setWithdrawAmount(withdraw);
+      }
+    }
+
+    fetchUserTransactions();
+  }, [wallet]);
+
   return (
-    <section>
-      Hello,
-      <h1>{user?.name}</h1>
-    </section>
+    <>
+      <section>
+        Hello,
+        <h1>{user?.name}</h1>
+      </section>
+      <section>
+        <ul>
+          <li>
+            <h5>Balance</h5>
+            <p>{wallet?.balance}</p>
+          </li>
+          <li>
+            <h5>Deposit</h5>
+            <p>{depositAmount}</p>
+          </li>
+          <li>
+            <h5>Withdraw</h5>
+            <p>{withdrawAmount}</p>
+          </li>
+        </ul>
+      </section>
+    </>
   );
 }
--- src/Pages/Homepage/HomePage.test.jsx
+++ src/Pages/Homepage/HomePage.test.jsx
@@ -1,10 +1,29 @@
 import axios from 'axios';
-import { customers } from '../../../fixtures.json';
-import { render, screen } from '@testing-library/react';
+import { customers, transactions, wallets } from '../../../fixtures.json';
+import {
+  findByText,
+  getAllByRole,
+  getByRole,
+  render,
+  screen
+} from '@testing-library/react';
 import HomePage from './HomePage';
 import { BACKEND_URL, CUSTOMER_ID } from '../../constants';
 
 jest.mock('axios');
+
+let customer;
+let wallet;
+
+beforeEach(() => {
+  customer = customers.find((customer) => customer.id === CUSTOMER_ID);
+  wallet = wallets.find((wallet) => wallet.id === customer.walletId);
+
+  axios.get
+    .mockResolvedValueOnce({ data: customer })
+    .mockResolvedValueOnce({ data: wallet })
+    .mockResolvedValueOnce({ data: { transactions } });
+});
 
 afterEach(() => {
   jest.resetAllMocks();
@@ -12,8 +31,6 @@ afterEach(() => {
 
 describe('HomePage', () => {
   it('should display greetings on homepage and "Hello," text', async () => {
-    const customer = customers.find((customer) => customer.id === CUSTOMER_ID);
-    axios.get = jest.fn().mockResolvedValue({ data: customer });
     render(<HomePage />);
     const helloTextElement = screen.getByText('Hello,');
     const greetingElement = await screen.findByRole('heading', {
@@ -23,9 +40,55 @@ describe('HomePage', () => {
 
     expect(helloTextElement).toBeInTheDocument();
     expect(greetingElement).toBeInTheDocument();
-    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledTimes(3);
     expect(axios.get).toHaveBeenCalledWith(
       `${BACKEND_URL}/customers/${CUSTOMER_ID}`
+    );
+  });
+
+  it('should render balance, deposit total and withdraw total', async () => {
+    render(<HomePage />);
+    const balanceInformation = screen.getByRole('list');
+    const [balanceElement, depositElement, withdrawElement] = getAllByRole(
+      balanceInformation,
+      'listitem'
+    );
+    const balanceTitle = getByRole(balanceElement, 'heading', {
+      level: 5,
+      name: /balance/i
+    });
+    const depositTitle = getByRole(depositElement, 'heading', {
+      level: 5,
+      name: /deposit/i
+    });
+    const withdrawTitle = getByRole(withdrawElement, 'heading', {
+      level: 5,
+      name: /withdraw/i
+    });
+    const balanceAmount = await findByText(balanceElement, wallet.balance);
+    const depositAmount = await findByText(
+      depositElement,
+      transactions[0].amount
+    );
+    const withdrawAmount = await findByText(
+      withdrawElement,
+      transactions[1].amount
+    );
+
+    expect(balanceTitle).toBeInTheDocument();
+    expect(depositTitle).toBeInTheDocument();
+    expect(withdrawTitle).toBeInTheDocument();
+    expect(balanceAmount).toBeInTheDocument();
+    expect(depositAmount).toBeInTheDocument();
+    expect(withdrawAmount).toBeInTheDocument();
+    expect(axios.get).toHaveBeenCalledTimes(3);
+    expect(axios.get).toHaveBeenNthCalledWith(
+      2,
+      `${BACKEND_URL}/wallets/${customer.walletId}`
+    );
+    expect(axios.get).toHaveBeenNthCalledWith(
+      3,
+      `${BACKEND_URL}/wallets/${customer.walletId}/transactions`
     );
   });
 });
