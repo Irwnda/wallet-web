af9249ee3adc5931ba25fb35569e733254999b5b
ACHMAD IRWANDA <achmad.irwanda@btpn.com>
2024-05-16 10:14:31 +0700

    style: add logo

--- src/Pages/Homepage/BalanceInformation.jsx
+++ src/Pages/Homepage/BalanceInformation.jsx
@@ -7,14 +7,17 @@ export default function BalanceInformation({ balance, deposit, withdraw }) {
         <li className="balance-information-card">
           <h5>Balance</h5>
           <p>{balance}</p>
+          <img src="/jenius-orange.png" alt="Jenius Logo Orange" />
         </li>
         <li className="deposit-information-card">
           <h5>Deposit</h5>
           <p>{deposit}</p>
+          <img src="/jenius-yellow.png" alt="Jenius Logo Yellow" />
         </li>
         <li className="withdraw-information-card">
           <h5>Withdraw</h5>
           <p>{withdraw}</p>
+          <img src="/jenius-blue.png" alt="Jenius Logo Blue" />
         </li>
       </ul>
     </section>
